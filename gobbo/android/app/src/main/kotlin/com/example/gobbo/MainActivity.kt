9b847ebfd6b508855623d0007407249c435356af
BhryanS2 <bhryanstepenhen@gmail.com>
2022-07-07 08:14:58 -0300

    del

--- gobbo/android/app/src/main/kotlin/com/example/gobbo/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.gobbo
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- inputtext/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:inputtext/fuel.dart';
-
-void main() {
-  // ignore: prefer_const_constructors
-  runApp(MaterialApp(
-    home: Fuel(),
-  ));
-}
